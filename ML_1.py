35410ff15605b3f966e7c75894e5949ac880d829
faizan-523 <faizan.7.ist@gmail.com>
2026-03-24 00:23:10 +0500

    Machine Learning

---
+++ ML_1.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import linear_model
+
+df = pd.read_excel('Linear_regression.xlsx')
+
+x = df[['Study Hours']]
+y = df[['Marks']]
+
+plt.scatter(x , y, color='blue', marker='o')
+plt.xlabel('Study Hours')
+plt.ylabel('Marks')
+plt.title('Linear Regression')
+plt.show()
+
+reg = linear_model.LinearRegression()
+reg.fit(x , y)
+
+print('Coefficient: ', reg.coef_)
+print('Intercept: ', reg.intercept_)
+
+predicted_marks = reg.predict([[12]])
+print('Predicted Marks for 12 Study Hours: ', predicted_marks)
+
+plt.scatter(x , y, color='blue', marker='o')
+plt.plot(x, reg.predict(x), color='red')
+plt.xlabel('Study Hours')
+plt.ylabel('Marks')
+plt.title('Linear Regression with Line of Best Fit')
+plt.show()
+
