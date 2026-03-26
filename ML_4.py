446083fd8200b0b3bbf32a43258f20928ce4369a
faizan-523 <faizan.7.ist@gmail.com>
2026-03-27 01:35:40 +0500

    Dummies variables

---
+++ ML_4.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+
+df = pd.read_excel('Linear_Regression.xlsx')
+
+d = pd.get_dummies(df.Names, dtype='int')
+print(d)
+
+merged = pd.concat([df, d], axis='columns')
+print(merged)
+
+final = merged.drop(['Names'], axis='columns')
+print(final)
+
+x = final.drop(['Marks'], axis='columns')
+y = final.Marks
+
+model.fit(x, y)
+pred = model.predict([[1,7,2,8,0,0,0,0,0,0,0,1,0,0]])
+print("Predicted Marks:", pred)
+
+print(model.score(x, y))
